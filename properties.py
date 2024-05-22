b50918875e02a8e557b0011edb05c0054c2304d6
radioworm <radioworm@rambler.ru>
2024-05-22 23:25:51 +0300

    Add files via upload
    add file properties.py

---
+++ properties.py
@@ -0,0 +1,6 @@
+auth_token = '4d1*******************************************6bc3'
+url = 'https://chatapi.viber.com/pa/send_message'
+conversation_url = 'viber://pa?chatURI=****************'
+headers = {'X-Viber-Auth-Token': auth_token}
+server_url = "https://***********.ru"
+app_uri = '/6***************'
